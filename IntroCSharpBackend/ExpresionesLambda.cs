2a66436802334ce5796e812901bd55c02e6bf46f
Daniel Rivera <danieldk8@gmail.com>
2024-10-15 18:54:52 -0300

    Primer commit

---
+++ IntroCSharpBackend/ExpresionesLambda.cs
@@ -0,0 +1,21 @@
+﻿
+//Func<int, int, int> sub = (a, b) => a - b;
+
+//Func<int, int> some = a => a * 2;
+
+//Func<int, int> some2 = a =>
+//{
+//    a = a + 1;
+//    return a * 5;
+//};
+
+//Console.WriteLine(some2(1));
+
+//-----------------------------------------------
+
+//Some((a, b) => a + b, 5);
+
+//void Some(Func<int, int, int> fn, int number)
+//{
+//    var resultado = fn(number, number);
+//}
---
+++ IntroCSharpBackend/Linq.cs
@@ -0,0 +1,19 @@
+﻿
+//var names = new List<string>()
+//{
+//    "Hector", "Francisco", "Ana", "Hugo", "Pedro"
+//};
+
+//var namesResult = from n in names
+//                  where n.Length > 3 && n.Length < 5
+//                  orderby n descending
+//                  select n;
+
+//var namesResult2 = names.Where(n => n.Length > 3 && n.Length < 5)
+//                    .OrderByDescending(n => n)
+//                    .Select(d => d);
+
+//foreach (var name in namesResult2)
+//{
+//    Console.WriteLine(name);
+//}
